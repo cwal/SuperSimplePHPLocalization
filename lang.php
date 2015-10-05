ad9381e73357bb72df3d5d420180b02e9d7b22ef
Christopher Waldau <cwal@users.noreply.github.com>
2015-10-05 09:19:04 -0400

    Add ability for a user defined default language

--- lang.php
+++ lang.php
@@ -51,34 +51,59 @@ defined('INCLUDE_PATH') or define('INCLUDE_PATH', dirname(__FILE__) . '/');
  */
 if(isset($_GET['lang']))
 	define('LANGUAGE', $_GET['lang']);
+elseif(defined('LANG_DEFAULT'))
+	define('LANGUAGE', LANG_DEFAULT);
 else
-	defined('LANGUAGE') or define('LANGUAGE', 'en');
+	define('LANGUAGE', 'en');
 
 /*
- * Load the proper language file and return the translated phrase
- *
- * The language file is JSON encoded and returns an associative array (must be UTF8 encoded)
+ * Verify that at least the default language file exists and populate $translations array
+ */
+$translations = get_translations();
+
+/*
+ * Return the translated phrase
  *
  * @param string $phrase The phrase that needs to be translated
  * @return string The translated string
  */
 function __($phrase) {
-    /* Static keyword is used to ensure the file is loaded only once */
-    static $translations = NULL;
-    /* If no instance of $translations has occured load the language file */
-    if (is_null($translations)) {
-        $lang_file = INCLUDE_PATH . LANGUAGE . '.txt';
-        if (!file_exists($lang_file)) {
-            $lang_file = INCLUDE_PATH . 'en.txt';
-        }
-        $lang_file_content = file_get_contents($lang_file);
-        /* Load the language file as a JSON object and transform it into an associative array */
-        $translations = json_decode($lang_file_content, true);
-    }
+	global $translations;
 	/* Check if the $phrase exists in the array, otherwise return just $phrase */
 	if(array_key_exists($phrase, $translations))
-    	return $translations[$phrase];
+		return $translations[$phrase];
 	else
 		return $phrase;
+}
+
+/*
+ * Load the proper language file and return the translations array
+ *
+ * The language file is JSON encoded and returns an associative array (must be UTF8 encoded)
+ * 
+ * @return array The translations
+ */
+function get_translations() {
+	/* Static keyword is used to ensure the file is loaded only once */
+	static $translations = NULL;
+	/* If no instance of $translations has occured load the language file */
+	if (is_null($translations)) {
+		$lang_file = INCLUDE_PATH . LANGUAGE . '.txt';
+		if (!file_exists($lang_file) && defined('LANG_DEFAULT')) { // requested language not found, try for user defined default
+			$lang_file = INCLUDE_PATH . LANG_DEFAULT . '.txt';
+		}
+		if (!file_exists($lang_file)) { // still not found, try for script default
+			$lang_file = INCLUDE_PATH . 'en.txt';
+		}
+		if (!file_exists($lang_file)) { // can't find requested, user defined or script default
+			echo '<h1>LANGUAGE FILE NOT FOUND!</h1>';
+			echo 'Please ensure you have included the necessary language files in ' . INCLUDE_PATH;
+			die();
+		}
+		$lang_file_content = file_get_contents($lang_file);
+		/* Load the language file as a JSON object and transform it into an associative array */
+		$translations = json_decode($lang_file_content, true);
+	}
+	return $translations;
 }
 ?>
